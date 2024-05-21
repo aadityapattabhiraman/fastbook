04536bf8d385fc15cd38080a375e3d5f76f02909
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-05-21 09:24:25 +0530

    Update mrk4.py

--- mrk4.py
+++ mrk4.py
@@ -8,3 +8,13 @@ from fastai.vision.all import *
 path = untar_data(URLs.MNIST_SAMPLE)
 Path.BASE_PATH = path
 print(path.ls())
+
+threes = (path / "train" / "3").ls().sorted()
+sevens = (path / "train" / "7").ls().sorted()
+print(threes)
+
+im3_path = threes[1]
+im3 = Image.open(im3_path)
+# im3.show()
+
+array(im3)[4: 10, 4: 10]
